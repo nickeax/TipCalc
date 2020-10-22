12679d68a66cd9bfac5eb8b9ec487a7e3155efc2
Nick Fletcher <nickeax@gmail.com>
2020-10-22 14:54:16 +1100

    Add project files.

---
+++ TipCalc.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using TipCalc.Core.Services;
+using TipCalc.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalc.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
